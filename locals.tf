a291715f49a39bd59267aa5be0a1ce1c7c0fbb75
Rohith Jayawardene <gambol99@gmail.com>
2024-06-13 13:16:53 +0100

    feat: ignoring the slack lambda if configuration not complete (#21)

--- locals.tf
+++ locals.tf
@@ -15,13 +15,15 @@ locals {
   sns_topic_policy = var.sns_topic_policy != null ? var.sns_topic_policy : data.aws_iam_policy_document.current.json
 
   ## Indicates if we are enabling slack notifications 
-  enable_slack = var.slack != null ? true : false
+  enable_slack_config = var.slack != null ? true : false
   ## Indicates if we are looking up the slack secret 
-  enable_slack_secret = local.enable_slack && try(var.slack.secret_name, null) != null ? true : false
+  enable_slack_secret = local.enable_slack_config && try(var.slack.secret_name, null) != null ? true : false
   ## The webhook url for slack 
   slack_webhook_url = local.enable_slack_secret ? try(jsondecode(data.aws_secretsmanager_secret_version.slack[0].secret_string)["webhook_url"], var.slack.webhook_url) : try(var.slack.webhook_url, null)
   ## The slack channel to post to 
   slack_channel = local.enable_slack_secret ? try(jsondecode(data.aws_secretsmanager_secret_version.slack[0].secret_string)["channel"], var.slack.channel) : try(var.slack.channel, null)
   ## slack_username to use  
   slack_username = local.enable_slack_secret ? try(jsondecode(data.aws_secretsmanager_secret_version.slack[0].secret_string)["username"], var.slack.username) : try(var.slack.username, null)
+  ## Indicates slack has all the configuration needed 
+  enable_slack = local.enable_slack_config && local.slack_channel != null && local.slack_webhook_url != null
 }
